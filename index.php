3cecfc83ccbbb045cfe02fcf980c3fa878e80f04
SENG Sourng <sourng2009@gmail.com>
2018-10-02 08:26:39 +0700

    lesson 01
    using form

---
+++ index.php
@@ -0,0 +1,14 @@
+<html> 
+ <head>    
+	<title>Personalized Greeting Form</title>  
+ </head>
+	<body>
+		 <?php if(!empty($_POST['name'])) {    
+			echo "Greetings, {$_POST['name']}, and welcome.";    } 
+		 ?>
+			<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">     
+				Enter your name: <input type="text" name="name" />     
+				<input type="submit" />    
+			</form>  
+	</body>
+</html>
